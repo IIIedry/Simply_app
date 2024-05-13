90c8fd9267fbd54120ff2a2eeab669062d4d1067
IIIedry <ilias.02.08@mail.ru>
2024-05-13 11:21:21 +0500

    java file for build app and create database

--- app/src/main/java/com/example/weather_forecast/di/component/AppComponent.kt
+++ app/src/main/java/com/example/weather_forecast/di/component/AppComponent.kt
@@ -1,5 +1,9 @@
 package com.example.weather_forecast.di.component
 
+import com.example.weather_forecast.di.module.AppModule
+import com.example.weather_forecast.di.module.DatabaseModule
+import com.example.weather_forecast.di.module.NetworkModule
+import com.example.weather_forecast.ui.MainActivity
 import dagger.Component
 import javax.inject.Singleton
 
